d5abfd73c7f6930cbf54d8416e2a0ee60f844e13
amanuel <azewgee@gmail.com>
2019-05-11 07:53:36 -0500

    new com

--- buggy3.js
+++ buggy3.js
@@ -1,7 +1,7 @@
 
-//there was typing mistake (;) after the end of the gogogo function which make to do nothing
+//there was typing mistake (;) after the end of the gogogo function
 function gogogo() {
     alert("Yay, it works!");
     //Alert------> alert
 }
-/// typographic error (  typing ) twice) than typin a closing brackt
+/// typographic error - typing ")" twice than typing a closing brackt
