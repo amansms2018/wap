f03f49eaefe4744fabdc6e7cd6d1ff2667f7e28a
amanuel <azewgee@gmail.com>
2019-05-11 00:41:05 -0500

    new com

---
+++ buggy3.js
@@ -0,0 +1,7 @@
+
+//there was typing mistake (;) after the end of the gogogo function which make to do nothing
+function gogogo() {
+    alert("Yay, it works!");
+    //Alert------> alert
+}
+/// typographic error (  typing ) twice) than typin a closing brackt
---
+++ buggy4.js
@@ -0,0 +1,13 @@
+function somethingElse() {
+    if (5 > 2) {
+        alert("Bigger");
+        if (5 < 2) {
+            alert("Smaller");
+        }
+    }
+
+}
+/// closing curly- bracket was missing
+function gogogo() {
+        alert("Yay, it works!");
+    }
